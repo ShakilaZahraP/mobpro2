3f28e3808492412f83245af29c7184a1038faa21
shakilazahrap <shakilazahra29@gmail.com>
2020-09-17 23:50:56 +0700

    initial commit

--- app/src/main/java/org/d3if4103/mobpro2/MainActivity.kt
+++ app/src/main/java/org/d3if4103/mobpro2/MainActivity.kt
@@ -2,11 +2,14 @@
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 
  class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ViewModelProvider(this).get(MainViewModel::class.java)
     }
 }
